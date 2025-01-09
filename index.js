8ba22ead078e630cad91fb26fb020b5ab7a7c44c
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2025-01-09 09:04:33 +0530

    fix: update CORS allowed origins to handle trailing slashes

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
-const allowedOrigins = ['https://chavindu-ransara.vercel.app/'];
+const allowedOrigins = ['https://chavindu-ransara.vercel.app'];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
 };
 
 app.use(cors(corsOptions));
